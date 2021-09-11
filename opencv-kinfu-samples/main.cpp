7e2d47ce4c37c1edbc02edf23c88cc16981e6c00
robin.xu <robinxu@ucsd.edu>
2021-09-10 18:44:18 -0700

    some cpp compile

--- opencv-kinfu-samples/main.cpp
+++ opencv-kinfu-samples/main.cpp
@@ -453,7 +453,7 @@ int main(int argc, char** argv)
 
 #ifdef HAVE_OPENCV
     //if it doesn't work, try set setUseOptimized(false);
-    setUseOptimized(false);
+    setUseOptimized(true);
 
     // Retrieve calibration parameters
     k4a_calibration_intrinsic_parameters_t *intrinsics = &calibration.depth_camera_calibration.intrinsics.parameters;
